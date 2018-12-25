fb2c177e012e404b69a12b67501353a3da57afdd
StanislavTrenin <strenin25@gmail.com>
2018-12-25 20:14:18 +0700

    labs for course destributed algoritms

---
+++ myChat/src/main/java/ChatDemo.java
@@ -0,0 +1,61 @@
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+import org.jgroups.jmx.JmxConfigurator;
+import org.jgroups.util.Util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ChatDemo extends ReceiverAdapter {
+    protected JChannel channel;
+
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        String payload = msg.getObject();
+        System.out.printf("[%s]: %s\n", msg.getSrc(), payload);
+    }
+
+
+    private void start(String props, String name) throws Exception {
+        channel = new JChannel(props).name(name).receiver(this);
+        channel.connect("ChatCluster");
+        JmxConfigurator.registerChannel(channel, Util.getMBeanServer(), "chat-channel", channel.getClusterName(), true);
+        eventLoop();
+        channel.close();
+    }
+
+    private void eventLoop() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("> ");
+                System.out.flush();
+                String line = in.readLine().toLowerCase();
+                if (line.startsWith("quit") || line.startsWith("exit"))
+                    break;
+                Message msg = new Message(null, line);
+                channel.send(msg);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String props = "config.xml";
+        String name = "User2";
+
+
+        try {
+            new ChatDemo().start(props, name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
